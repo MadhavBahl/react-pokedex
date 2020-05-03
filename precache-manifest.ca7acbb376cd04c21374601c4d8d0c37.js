4b7d41ae1b3f2cd9c16fd8e0a30a5e9be1362a6f
MadhavBahlMD <madhavbahl10@gmail.com>
2020-05-03 21:39:27 +0530

    Updates

---
+++ precache-manifest.ca7acbb376cd04c21374601c4d8d0c37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c39ad96b191efd1086dd1209d1124908",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "74fc77645b1aae157f71",
+    "url": "/react-pokedex/static/css/main.e8145454.chunk.css"
+  },
+  {
+    "revision": "da5e2ed3d20e3e85c998",
+    "url": "/react-pokedex/static/js/2.947f6efe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pokedex/static/js/2.947f6efe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74fc77645b1aae157f71",
+    "url": "/react-pokedex/static/js/main.e6766691.chunk.js"
+  },
+  {
+    "revision": "37bd2673ee8786092a34",
+    "url": "/react-pokedex/static/js/runtime-main.759a80a8.js"
+  }
+]);
